4440da00ae3c317cd90fca68f4a214e5a29c0965
SecretProtonium <SecretProtonium@pm.me>
2019-11-22 15:44:44 +0100

    Add conditional comment for ie9 elementor popup style

--- src/header.php
+++ src/header.php
@@ -12,6 +12,13 @@
         window.location = "http://whatbrowser.org/intl/de/";
     </script>
     <![endif]-->
+
+    <!--[if IE 9]>
+    <style>
+        .elementor-popup-modal { display: block !important; }
+        .elementor-popup-modal > div { left: 50%; transform: translateX(-50%); }
+    </style>
+    <![endif]-->
 </head>
 <body <?php body_class( 'no-js' ); ?>>
 <script>
