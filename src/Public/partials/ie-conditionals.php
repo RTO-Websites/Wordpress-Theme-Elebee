d24032eb548fee90b39beb3d1dcd057c147da8d9
SecretProtonium <SecretProtonium@pm.me>
2019-11-25 11:40:08 +0100

    Render ie conditionals even if a custom header is supplied.

---
+++ src/Public/partials/ie-conditionals.php
@@ -0,0 +1,18 @@
+<!--[if lt IE 9]>
+<script type="text/javascript">
+    window.location = "http://whatbrowser.org/intl/de/";
+</script>
+<![endif]-->
+
+<!--[if IE 9]>
+<style>
+    .elementor-popup-modal {
+        display: block !important;
+    }
+
+    .elementor-popup-modal > div {
+        left: 50%;
+        transform: translateX(-50%);
+    }
+</style>
+<![endif]-->
