b615bf86ef3eb67423a166e9f62dffde20f276d4
Sascha Hennemann <s.hennemann@rto.de>
2020-02-07 16:40:54 +0100

    Remove double embed of ie conditionals

--- src/header.php
+++ src/header.php
@@ -6,19 +6,6 @@
             content="width=device-width, initial-scale=1.0">
 
     <?php wp_head(); ?>
-
-    <!--[if lt IE 9]>
-    <script type="text/javascript">
-        window.location = "http://whatbrowser.org/intl/de/";
-    </script>
-    <![endif]-->
-
-    <!--[if IE 9]>
-    <style>
-        .elementor-popup-modal { display: block !important; }
-        .elementor-popup-modal > div { left: 50%; transform: translateX(-50%); }
-    </style>
-    <![endif]-->
 </head>
 <body <?php body_class( 'no-js' ); ?>>
 <script>
